8977dd70b0fcf832acdde8c2e038fa3488bc941b
Adryan Alencar <adryan.alencar@sevenfox.com.br>
2022-03-21 04:52:02 -0300

    Ticker for runtime

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 require('dotenv').config()
 import { app } from './app';
 import { logger } from './services/logger';
+import { startTick } from './services/tick';
 import { initWorker } from './services/worker';
 
 app.listen(process.env.PORT || 8080, () => {  
     logger.info(`[HTTP] => Server is running on port ${process.env.PORT || 8080}`);
-    initWorker();      
+    initWorker();
+    startTick();
 });
---
+++ src/services/tick.ts
@@ -0,0 +1,25 @@
+import { logger } from "./logger";
+
+process
+    .on('SIGTERM', shutdown('SIGTERM'))
+    .on('SIGINT', shutdown('SIGINT'))
+    .on('uncaughtException', shutdown('uncaughtException'));
+
+
+export function startTick(){
+    setInterval(() => {
+        logger.info("Tick")
+    }, 1000);
+}
+
+
+function shutdown(signal : string) {
+    return (err : any) => {
+        logger.warn(`${signal}...`);
+        if (err) logger.error(err.stack || err);
+        setTimeout(() => {
+            logger.warn('...waited 5s, exiting.');
+            process.exit(err ? 1 : 0);
+        }, 5000).unref();
+    };
+}
